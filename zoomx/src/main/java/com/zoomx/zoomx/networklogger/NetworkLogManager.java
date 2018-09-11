cd1614f2d8c96096298163d39f80046faac26b2f
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2018-09-12 01:19:54 +0200

    add nonNull annotation

--- zoomx/src/main/java/com/zoomx/zoomx/networklogger/NetworkLogManager.java
+++ zoomx/src/main/java/com/zoomx/zoomx/networklogger/NetworkLogManager.java
@@ -1,5 +1,6 @@
 package com.zoomx.zoomx.networklogger;
 
+import android.support.annotation.NonNull;
 import com.zoomx.zoomx.config.ZoomX;
 import com.zoomx.zoomx.model.RequestEntity;
 import io.reactivex.Completable;
@@ -7,15 +8,17 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
 
+import java.util.Objects;
+
 /**
  * Created by Ahmed Fathallah on 1/11/2018.
  */
 
 public class NetworkLogManager {
 
-    public static void log(RequestEntity requestEntity) {
-        if (requestEntity != null)
-            insertRequest(requestEntity).subscribe();
+    public static void log(@NonNull RequestEntity requestEntity) {
+        Objects.requireNonNull(requestEntity);
+        insertRequest(requestEntity).subscribe();
     }
 
     private static Completable insertRequest(RequestEntity requestEntity) {
